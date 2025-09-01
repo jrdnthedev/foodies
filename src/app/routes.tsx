a050017e9ac02b35138b3a74d1192b6a26194bb3
Andre McMorris <ajmcmorris@gmail.com>
2025-09-01 11:20:49 -0400

    feat: added new route

--- src/app/routes.tsx
+++ src/app/routes.tsx
@@ -1,6 +1,7 @@
 import { lazy, type JSX } from 'react';
 
 const VendorDashboard = lazy(() => import('../domains/vendor/ui/dashboard/dashboard'));
+const VendorProfile = lazy(() => import('../domains/vendor/ui/profile/profile'));
 // const About = lazy(() => import("../pages/About"));
 
 export const ProtectedRoutes: ProtectedRoute[] = [
@@ -13,6 +14,11 @@ export const ProtectedRoutes: ProtectedRoute[] = [
     path: '/vendor-dashboard',
     element: <VendorDashboard />,
     label: 'Dashboard',
+  },
+  {
+    path: '/vendor/:vendorId',
+    element: <VendorProfile />,
+    label: 'Vendor Profile',
   },
 ];
 
