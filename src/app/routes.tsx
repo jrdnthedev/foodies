849f1c5bba36fb79c89a6814d9e298f2c0a09313
Andre McMorris <ajmcmorris@gmail.com>
2025-09-03 14:44:29 -0400

    refactor: enhancements

--- src/app/routes.tsx
+++ src/app/routes.tsx
@@ -1,8 +1,9 @@
 import { lazy, type JSX } from 'react';
 
 const VendorDashboard = lazy(() => import('../domains/vendor/ui/dashboard/dashboard'));
-const VendorProfile = lazy(() => import('../domains/vendor/ui/profile/profile'));
+const VendorProfile = lazy(() => import('../domains/discovery/ui/profile/profile'));
 const DiscoveryDashboard = lazy(() => import('../domains/discovery/ui/dashboard/dashboard'));
+// const DiscoveryPage = lazy(() => import('../domains/discovery/ui/discovery-page/DiscoveryPage'));
 
 export const ProtectedRoutes: ProtectedRoute[] = [
   {
